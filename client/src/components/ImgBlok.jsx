c930d9af31cef6f1deb789b4b54cd50e8e665421
Alllena <khatkevich.alena@gmail.com>
2023-03-28 08:54:09 +0300

    add InfoBlok

---
+++ client/src/components/ImgBlok.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { FlexContainer } from "./styled/FlexContainer";
+
+const ImgBlok = ({ images }) => {
+  const [isMain, setIsMain] = useState("");
+  const [isActive, setIsActive] = useState(Number);
+  useEffect(() => {
+    getMainImage();
+  }, []);
+
+  const getMainImage = () => {
+    const mainImage = images.filter((image) => {
+      return image.isMain === true;
+    });
+    setIsMain(mainImage[0].url);
+    setIsActive(mainImage[0].id);
+  };
+
+  return (
+    <Wrapper>
+      <img className="img__main" src={isMain} alt="a" />
+      <FlexContainer className="img__blok" direction="column">
+        {images.map(
+          (image) =>
+            isActive === image.id ? (
+              <img
+                key={image.id}
+                className="img__item active"
+                src={image.url}
+                alt="img"
+              />
+            ) : (
+              <img
+                key={image.id}
+                className="img__item"
+                src={image.url}
+                alt="img"
+                onClick={() => {
+                  setIsMain(image.url);
+                  setIsActive(image.id);
+                }}
+              />
+            )
+          // <ImgItem className="img__item" name={image.url} />
+        )}
+      </FlexContainer>
+    </Wrapper>
+  );
+};
+export default ImgBlok;
+
+const Wrapper = styled.div`
+  flex: 2;
+  display: flex;
+  .img__main {
+    max-height: 90vh;
+    max-width: 90vh;
+  }
+  .img__blok {
+    width: 40%;
+    .img__item {
+      height: 120px;
+      width: 120px;
+      &.active {
+        border: 1px solid #4096ff;
+        background-color: #4096ff19;
+      }
+    }
+  }
+`;
+
+// const ImgItem = styled.div`
+// height: 120px;
+// width: 120px;
+//   /* background: url(${({ name }) => name}); */
+// `;
