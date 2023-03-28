92042a31b22b5549fca8aff283a9a81479a604c3
Alllena <khatkevich.alena@gmail.com>
2023-03-28 08:55:06 +0300

    add InfoBlok

---
+++ client/src/components/InfoBlok.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import ColorBlok from "./ColorBlok";
+import SizeBlok from "./SizeBlok";
+import { FlexContainer } from "./styled/FlexContainer";
+
+const InfoBlok = ({
+  id,
+  name,
+  price,
+  sales,
+  color,
+  types,
+  colors,
+  sizes,
+  info,
+}) => {
+  return (
+    <Wrapper>
+      <h2>
+        {name} {color}
+      </h2>
+      {types.map((type) => (
+        <h3>
+          {type.name} {name} {color}
+        </h3>
+      ))}
+      <FlexContainer className="product__prase" justify="start">
+        {sales > 0 ? (
+          <FlexContainer justify="start">
+            <div className="price price-discount">
+              €{price - (sales / 100) * price}
+            </div>
+            <div className="price price-before">€{price}</div>
+            <FlexContainer className="sales">-{sales}%</FlexContainer>
+          </FlexContainer>
+        ) : (
+          <div className="price">€{price}</div>
+        )}
+      </FlexContainer>
+      <ColorBlok colors={colors} />
+      <SizeBlok sizes={sizes} />
+
+      <p>{info}</p>
+    </Wrapper>
+  );
+};
+
+export default InfoBlok;
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: start;
+  align-items: start;
+  flex-direction: column;
+  height: 100%;
+`;
