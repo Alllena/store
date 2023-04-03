ec46a3357a0aab15bbf1d15f4f60e0de9e6f442a
Alllena <khatkevich.alena@gmail.com>
2023-04-03 08:23:42 +0300

    added provider store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
+import { setupStore } from "./store/store";
+
+const store = setupStore();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App></App>);
+root.render(
+  <Provider store={store}>
+    <App></App>
+  </Provider>
+);
