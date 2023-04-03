1d47f3ad48f22ac82ac4edfe655a17ffbaaf970d
Alllena <khatkevich.alena@gmail.com>
2023-04-03 23:43:02 +0300

    added Couter component

---
+++ client/src/components/Couter.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import Button, { ButtonLook } from "./base/Buttons";
+import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
+import { FlexContainer } from "./styled/FlexContainer";
+
+const Counter = () => {
+  return (
+    <CounterWrapper>
+      <Button onClick={() => {}} look={ButtonLook.main}>
+        <PlusOutlined />
+      </Button>
+      <FlexContainer className="count"></FlexContainer>
+      <Button onClick={() => {}} look={ButtonLook.main}>
+        <MinusOutlined />
+      </Button>
+      <FlexContainer className="count__prase" justify="start">
+        €<span></span>
+      </FlexContainer>
+    </CounterWrapper>
+  );
+};
+export default Counter;
+
+const CounterWrapper = styled.div`
+  display: flex;
+  gap: 5px;
+  font-size: 22px;
+  .count {
+    width: 70px;
+  }
+  .count,
+  .count__prase {
+    height: 50px;
+    border-bottom: 1px solid rgba(160, 160, 160, 0.25);
+  }
+  .count__prase {
+    width: 100px;
+  }
+  button {
+    padding: 5px;
+  }
+`;
