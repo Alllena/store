1664c4fb1315253ca3821d63a4eae92748067414
Alllena <khatkevich.alena@gmail.com>
2023-04-09 07:50:26 +0300

    temporary changes

--- server/routes/productsRouter.js
+++ server/routes/productsRouter.js
@@ -4,7 +4,7 @@ const productController = require("../controllers/productController");
 const checkRole = require("../middleware/checkRoleMiddleware");
 
 router.post("/", productController.create);
-router.post("/", checkRole("ADMIN"), productController.create);
+// router.post("/", checkRole("ADMIN"), productController.create);
 router.get("/", productController.getAll);
 
 module.exports = router;
