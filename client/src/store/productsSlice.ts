3b39d44927b536693afa3d7ba95b9864ddd31603
Alllena <khatkevich.alena@gmail.com>
2023-04-10 01:29:08 +0300

    refactor

--- client/src/store/productsSlice.ts
+++ client/src/store/productsSlice.ts
@@ -1,24 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IProduct } from "../models/IProducts";
 
-// const initialState: IProduct[] = [];
-
-// export const productSlice = createSlice({
-//   name: "product",
-//   initialState,
-//   reducers: {
-//     addItem: (state, action: PayloadAction<IProduct>) => {
-//       state.push(action.payload);
-//     },
-//     removeItem: (state, action: PayloadAction<{ id: number }>) => {
-//       return state.filter((p) => p.id !== action.payload.id);
-//     },
-//   },
-// });
-
-// export const productReducer = productSlice.reducer;
-// export const productAction = productSlice.actions;
-
 interface IProductState {
   products: IProduct[];
   isLoading: boolean;
