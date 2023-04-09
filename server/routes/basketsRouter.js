23181e6414b2b35300636afc8f032b78118455a0
Alllena <khatkevich.alena@gmail.com>
2023-04-09 23:16:21 +0300

    deleted node_modules

--- server/routes/basketsRouter.js
+++ server/routes/basketsRouter.js
@@ -3,6 +3,7 @@ const router = new Router();
 const basketController = require("../controllers/basketController");
 
 router.post("/", basketController.create);
+router.post("/update/", basketController.update);
 router.get("/", basketController.getAll);
 
 module.exports = router;
