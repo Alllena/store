88bf5415d27912b0377ffe2c8b447e81ed24cf2e
Alllena <khatkevich.alena@gmail.com>
2023-04-09 09:52:15 +0300

    replacement of image files

--- server/routes/imgRouter.js
+++ server/routes/imgRouter.js
@@ -3,7 +3,7 @@ const router = new Router();
 const imgController = require("../controllers/imgController");
 const checkRole = require("../middleware/checkRoleMiddleware");
 
-router.post("/", checkRole("ADMIN"), imgController.create);
+// router.post("/", checkRole("ADMIN"), imgController.create);
 router.post("/", imgController.create);
 router.get("/", imgController.getAll);
 
